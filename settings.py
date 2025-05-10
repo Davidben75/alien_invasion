1ce35c69b581465af0f3d9a0263ff321867c1e8b
david <david>
2025-05-10 19:02:23 +0200

    change ship speed factor

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ class Settings():
         self.title = "Alien Invasion"
         
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 2
 
         # Bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
